799f10c5c1afe4bd55b0abb8f61a0aafd73eea96
Jonatan Martín Merinero <jmmerinero@gmail.com>
2015-06-11 18:30:34 +0200

    Refactor over implementation. [G] -> [R] -> [G]

--- src/mergesort.rb
+++ src/mergesort.rb
@@ -3,37 +3,25 @@ class Array
   def merge(left, right, &block)
     result = []
 
-#    left.each do |a|
-#      if(block_given?)
-        	
-
-
     while(!left.empty? and !right.empty?) do
-      #p block_given?
       comp = (block_given? ? block.call(left.first, right.first) : left.first <=> right.first)
-      #p comp
       (comp < 0) ? result << left.shift : result << right.shift
     end
 
     return result +left + right
   end
 
-  def mergesort_split(&block)
+  def mergesort(&block)
     if(self.size <= 1)
       return self
     end
 
     left = self[0...-(self.size/2)]
-    left = left.mergesort_split(&block)
+    left = left.mergesort(&block)
     right = self[-(self.size/2)..-1]
-    right = right.mergesort_split(&block)
+    right = right.mergesort(&block)
     
-    return merge(left, right, &block)
-  end
-
-  def mergesort(&block)
-    ordered_ary = self.mergesort_split(&block)
-    self.replace(ordered_ary)
+    self.replace(merge(left, right, &block))
   end
 
 end
