91d7f5e3421827ae3728a8ed2db669b2f1ab42be
Jonatan Martín Merinero <jmmerinero@gmail.com>
2015-06-11 23:04:33 +0200

    Refactor & comment. [G] -> [R] -> [G]

--- src/mergesort.rb
+++ src/mergesort.rb
@@ -1,27 +1,45 @@
 class Array
 
-  def merge(left, right, &block)
-    result = []
+def merge(left, right, &block)
+  result = []
+  (block = lambda {|a, b| a<=>b}) unless block_given?
 
-    while(!left.empty? and !right.empty?) do
-      comp = (block_given? ? block.call(left.first, right.first) : left.first <=> right.first)
-      (comp < 0) ? result << left.shift : result << right.shift
+  while(!left.empty? and !right.empty?) do
+    if (block.call(left.first, right.first) <= 0)
+      result << left.shift #.shift is destructive, .first isn't
+    else
+      result << right.shift
     end
-
-    return result +left + right
   end
 
-  def mergesort(&block)
-    if(self.size <= 1)
-      return self
-    end
+  # while loop doesn't empty both arys
+  return result + left + right
+end
 
-    left = self[0...-(self.size/2)]
-    left = left.mergesort(&block)
-    right = self[-(self.size/2)..-1]
-    right = right.mergesort(&block)
-    
-    self.replace(merge(left, right, &block))
+def mergesort(&block)
+  if(self.size <= 1)
+    return self
   end
+
+  # [-5|-4 |-3 |-2 |-1 ]
+  # [a , b , c , d , e ]  size = 5; size/2 = 2
+  # [0 | 1 | 2 | 3 | 4 ]
+  # [a , b , c ]         = [0...-2]
+  #             [d , e ] = [-2..-1]
+  #
+  # [-4|-3 |-2 |-1 ] 
+  # [a , b , c , d ]  size = 4; size/2 = 2
+  # [0 | 1 | 2 | 3 ]
+  # [a , b ]         = [0...-2]
+  #        [ e , d ] = [-2..-1]
+
+  left = self[0...-(self.size/2)]
+  right = self[-(self.size/2)..-1]
+  
+  left.mergesort(&block)
+  right.mergesort(&block)
+  
+  return self.replace(merge(left, right, &block))
+end
 
 end
--- test/mergesort_test.rb
+++ test/mergesort_test.rb
@@ -3,20 +3,11 @@ require_relative '../src/mergesort'
 
 RSpec.describe 'This is a mergesort implementation written in Ruby.' do
 
-=begin
-  class Array
-    def mergesort
-      self
-    end
-  end
-=end
-
   before(:all) do
   end
 
   context 'The method .mergesort works over Array objects' do
     it 'is answered when it is called over Array Objects' do
-      p Array.new().respond_to?(:mergesort)
       expect(Array.new().respond_to?(:mergesort)).to be true
     end
 
