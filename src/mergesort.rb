30ce6b85ff6d8f2048553c7c36a9bfe6265016b7
Jonatan Martín Merinero <jmmerinero@gmail.com>
2015-06-11 13:23:11 +0200

    Orders Array objects filled with Fixnum objects. [RED] -> [GREEN]

--- src/mergesort.rb
+++ src/mergesort.rb
@@ -1,5 +1,31 @@
 class Array
   def mergesort
-    self
+    #self = mergesort_split(self)
+    #self
+
+    self.sort!
+  end
+
+  def self.mergesort_split(ary)
+    if(ary.size == 1)
+      return ary
+    end
+
+    left = mergesort_split(ary[0, -(ary.size/2)])
+    right = mergesort_split(ary[-1, -(ary.size/2)])
+    
+    ary = merge(left, right)
+
+    return ary
+  end
+
+  def self.merge(left, right)
+    result = []
+
+    while(!left.empty? and !rigth.empty?) do
+      (left.first < right.first) ? result << left.shift : result << right.shift
+    end
+
+    return result
   end
 end
