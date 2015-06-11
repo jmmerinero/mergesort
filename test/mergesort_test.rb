d9948fa732dfb8f0a52258bb3db6dbd4557ba83f
Jonatan Martín Merinero <jmmerinero@gmail.com>
2015-06-11 12:47:17 +0200

    First round of tests passed. [GREEN]

--- test/mergesort_test.rb
+++ test/mergesort_test.rb
@@ -3,12 +3,21 @@ require_relative '../src/mergesort'
 
 RSpec.describe 'This is a mergesort implementation written in Ruby.' do
 
+=begin
+  class Array
+    def mergesort
+      self
+    end
+  end
+=end
+
   before(:all) do
   end
 
   context 'The method .mergesort works over Array objects' do
     it 'is answered when it is called over Array Objects' do
-      expect(Array.new().answer_to?(:mergesort))
+      p Array.new().respond_to?(:mergesort)
+      expect(Array.new().respond_to?(:mergesort)).to be true
     end
 
     it 'does nothing when applied over an empty Array object' do
